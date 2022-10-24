5c96b244c96a5a6eb4bf0eac18fc70094275bc28
imtiaz <imtiaz005123@gmai.com>
2022-10-24 14:10:33 +0600

    1st problem solve.

---
+++ src/O1_easy/O1_AddTwoIntegers_2235.java
@@ -0,0 +1,16 @@
+package O1_easy;
+
+public class O1_AddTwoIntegers_2235 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.sum(5,8));
+    }
+}
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
+
+// https://leetcode.com/problems/add-two-integers/
